df9d10ea2c423192a2a2836b9c71b302e8295d79
ptchipoc <pedrotchipoco@gmail.com>
2026-04-21 16:57:25 +0100

    Refactor email and phone extraction logic and update test script to utilize the new function

--- src/extractor.py
+++ src/extractor.py
@@ -1,3 +1,18 @@
-import re
+from re import findall
 
-def extract_email_phone(text: str) -> str:
+
+
+def extract_email_phone(text: str) -> dict[list[str]]:
+    email_regex = r'[\w\.-]+@[\w\.-]+\.\w+'
+    phone_regex = r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]'
+
+    emails = findall(email_regex, text)
+    phones = findall(phone_regex, text)
+
+    return {
+        "emails": emails,
+        "phones": phones
+    }
+
+# r'[\w\.-]+@[\w\.-]+\.\w+'
+# r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]'
--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,12 @@
 import sys
 from src.utils import load_text
-
+from src.extractor import extract_email_phone
 
 if __name__ == "__main__":
     argv = sys.argv
     if len(argv) < 2:
         print("Args should be more the one")
         exit(1)
-    print(load_text(argv[1]))
+    text = load_text(argv[1])
+    clean_content = extract_email_phone(text)
+    print(clean_content)
