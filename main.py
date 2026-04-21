3576bec94a66a6a72d7b0d3cc523823b150dd7e1
ptchipoc <pedrotchipoco@gmail.com>
2026-04-21 18:15:33 +0100

    Add main script to load text and extract emails and phones

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from src.utils import load_text
+from src.extractor import extract_email_phone
+from src.transform import transform
+
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) < 2:
+        exit(1)
+    text = load_text(argv[1])
+    clean_content = extract_email_phone(text)
+    transform(clean_content["emails"], clean_content["phones"])
